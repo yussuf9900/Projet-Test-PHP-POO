aad4f583a0a1ce4f9afd41a0b2714ea18fe503f8
yussuf9900 <yussuf9900@gmail.com>
2026-08-31 10:48:25 +0000

    feat(core): création de la classe mère et de l'entité Commande

---
+++ src/Core/AbstractEntity.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Core;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+
+abstract class AbstractEntity
+{
+    protected ?int $id;
+    protected ?DateTimeInterface $dateCreation;
+
+    public function __construct(?int $id = null, ?DateTimeInterface $dateCreation = null)
+    {
+        $this->id = $id;
+        $this->dateCreation = $dateCreation ?? new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getDateCreation(): ?DateTimeInterface
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(DateTimeInterface $dateCreation): void
+    {
+        $this->dateCreation = $dateCreation;
+    }
+}
